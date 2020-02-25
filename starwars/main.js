11bb9adf1ef63768de7ea38fecaa386b706e128c
Spencersandford <10735471@my.uvu.edu>
2020-02-25 12:41:41 -0700

    Adding Click Events

--- starwars/main.js
+++ starwars/main.js
@@ -1,5 +1,7 @@
 import { films } from '../data/films.js'
 import { people } from '../data/people.js'
+import {starships } from'../data/starships.js'
+
 
 const greetingDiv = document.querySelector(".greeting")
 
@@ -13,15 +15,27 @@ castList.appendChild(listItem1)
 
 let counter = 1
 
-
-
 people.forEach(person => {
     let listItem = document.createElement ("li")
     listItem.textContent = person.name
     castList.appendChild(listItem)
+
+
+    let anchorWrap = document.createElement ("a")
+    anchorWrap.href ="#"
+    
     let imageItem = document.createElement("img")
     imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
     greetingDiv.appendChild(imageItem)
+    
+    // add some way to handle user cliks on the image
+    imageItem.addEventListener("click", () => {
+        console.log("IT Worked!")
+    })
+    
+    
+    anchorWrap.appendChild(imageItem)
+    greetingDiv.appendChild(anchorWrap)
     counter++
 } )
 
