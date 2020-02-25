2f55bdd15c2ce0d3a597dbd084457cb1702b4bcd
Spencersandford <10735471@my.uvu.edu>
2020-02-25 12:22:49 -0700

    Importing Images

--- starwars/main.js
+++ starwars/main.js
@@ -1,15 +1,28 @@
 import { films } from '../data/films.js'
-
-console.log ('Hi There! Welcome to my page')
-
-console.log(document.querySelector(".greeting"))
+import { people } from '../data/people.js'
 
 const greetingDiv = document.querySelector(".greeting")
 
-greetingDiv.textContent = 'I just inserted text into a dom element using my mad JavaScript Skill!!'
+const castList = document.createElement ("ul")
 
-console.log(greetingDiv.textContent)
+const listItem1 = document.createElement ("li")
 
-console.log(films)
+listItem1.textContent = people[0].name
 
-greetingDiv.textContent = films[0].opening_crawl
+castList.appendChild(listItem1)
+
+let counter = 1
+
+
+
+people.forEach(person => {
+    let listItem = document.createElement ("li")
+    listItem.textContent = person.name
+    castList.appendChild(listItem)
+    let imageItem = document.createElement("img")
+    imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+    greetingDiv.appendChild(imageItem)
+    counter++
+} )
+
+greetingDiv.appendChild(castList)
