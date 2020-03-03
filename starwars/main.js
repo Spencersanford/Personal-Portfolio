5ed39f95e138fe8b1ccaa7abd95c87aeff044fb4
Spencersandford <10735471@my.uvu.edu>
2020-03-03 11:36:30 -0700

    Adding Css

--- starwars/main.js
+++ starwars/main.js
@@ -5,21 +5,29 @@ import {starships } from'../data/starships.js'
 
 const greetingDiv = document.querySelector(".greeting")
 
+const maleButton = document.querySelector('#maleButton')
+const otherButton = document.querySelector('#otherButton')
+const femaleButton = document.querySelector('#femaleButton')
+
+const maleCharacters = people.filter(person => person.gender === "male")
+console.log(maleCharacters)
+
+const femaleCharacters = people.filter(person => person.gender === "female")
+
+const otherCharacters = people.filter (person => {
+    if (person.gender === "hermaphrodite" || person.gender === "n/a") {
+        return person
+    }
+})
+
+
 const castList = document.createElement ("ul")
 
 const listItem1 = document.createElement ("li")
 
-listItem1.textContent = people[0].name
-
-castList.appendChild(listItem1)
-
 let counter = 1
 
 people.forEach(person => {
-    let listItem = document.createElement ("li")
-    listItem.textContent = person.name
-    castList.appendChild(listItem)
-
 
     let anchorWrap = document.createElement ("a")
     anchorWrap.href ="#"
@@ -30,6 +38,7 @@ people.forEach(person => {
 
     imageItem.addEventListener('error', (event) => {
         imageItem.hidden = true
+        //imageItem.src = '../Images/Uvu.png'
     })
     
     // add some way to handle user cliks on the image
@@ -44,3 +53,8 @@ people.forEach(person => {
 } )
 
 greetingDiv.appendChild(castList)
+
+maleButton.addEventListener("click", (event) => {
+    
+
+})
