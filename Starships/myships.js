900fe2ee43c45dd58d5172cfcb0e3aadaac47c2e
Spencersandford <10735471@my.uvu.edu>
2020-03-10 17:33:45 -0600

    Fixing the display of the starships

--- Starships/myships.js
+++ Starships/myships.js
@@ -1,16 +1,35 @@
 import { starships } from "../data/starships.js";
+import { getLastNumber, removeChildren } from "/utils.js"; 
+
 
 const nav = document.querySelector ('.nav')
 
 const navList = document.querySelector('.navlist')
+
+const shipView = document.querySelector('.main')
+
+const dialog = document.querySelector ('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+
+closeButton.addEventListener ('click', () => {
+  dialog.classList.toggle ('is-Active')
+})
+
+modalBackground.addEventListener ('click', () => {
+  dialog.classList.toggle ('is-Active')
+})
 
 
 function populateNav(starships) {
     starships.forEach(starship => {
       let anchorWrap = document.createElement("a");
       anchorWrap.href = "#";
+     
       anchorWrap.addEventListener("click", () => {
-        console.log("IT Worked!");
+        let shipName = event.target.textContent
+        const foundShip = starships.find(ship => ship.name === shipName);
+        populateShipView (foundShip)
       });
   
 
@@ -23,4 +42,21 @@ function populateNav(starships) {
     });
   }
 
+
+  function populateShipView (shipdata) {
+    removeChildren(shipView)
+  let shipNum = getLastNumber(shipdata.url)
+  let shipImage = document.createElement("img");
+  shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`;
+  
+  shipImage.addEventListener("error", event => {
+    //imageItem.hidden = true;
+    //imageItem.src = '../Images/Uvu.png'
+    shipImage.hidden = true
+    dialog.classList.toggle('is-active')
+  });
+  shipView.appendChild(shipImage);
+  }
+
   populateNav(starships)
+  
